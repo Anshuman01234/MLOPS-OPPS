decea5db4e1361f492d71006d89d77631b817af2
Anshuman01234 <anshumandas235@gmail.com>
2025-11-12 08:27:24 +0530

    project - signin updated

--- opps_proj.py
+++ opps_proj.py
@@ -13,14 +13,36 @@ class chatbook:
                            4. Press 4 to message a friend
                            5. Press any other key to exit''')
         if user_input == "1":
-            pass
+            self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
             pass
         elif user_input == "4":
             pass
         else:
             exit()
-
+    
+    def signup(self):
+        email = input("Enter your email id Here -> ")
+        password = input("SetUp  your password Here -> ")
+        self.username = email
+        self.password = password
+        print("Signup Successful!!")
+        print("\n")
+        self.menu()
+    
+    def signin(self):
+        if self.username == '' and self.password == '':
+            print("No user found, please signup first")
+        else:
+            usernm = input("Enter your email id Here -> ")
+            passwd = input("Enter your password Here -> ")
+            if usernm == self.username and passwd == self.password:
+                print("You have successfully logged in")
+                self.loggedin = True
+            else:
+                print("Invalid Credentials, please try again")
+        print("\n")
+        self.menu()
 obj = chatbook()
